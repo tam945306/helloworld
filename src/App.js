e702369857781eb0a59ee77a1558975b184ef56a
marcotamwork <marcotam.work@gmail.com>
2025-08-09 15:11:31 -0700

    Initial commit: React Hello World app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World!</h1>
+        <p>Welcome to your React app!</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
